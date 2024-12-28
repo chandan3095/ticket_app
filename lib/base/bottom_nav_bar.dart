be09bc83c28a8ac97c7dc6b7c1bf32316610c7cf
chandan3095 <chandanghosh593@gmail.com>
2024-12-28 15:24:29 +0530

    first commit

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,63 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/screen/home.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  final navList = [
+    const HomePage(),
+    const Text('Search'),
+    const Text('Tickets'),
+    const Text('Profile'),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: navList[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTapped,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Color(0xff526400),
+        showSelectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            label: 'Home',
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+            label: 'Search',
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+            label: 'Tickets',
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+            label: 'Profile',
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/bottom_nav_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BottomNavigation(),
+    );
+  }
+}
